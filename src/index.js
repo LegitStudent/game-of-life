b8df3f2ce73173a536a33b0ff90a77435e8762db
Jasper Francisco <jasper.francisco@gmail.com>
2017-02-26 19:44:00 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Grid from './components/Grid';
+import Game from './game/game';
+import './index.css';
+
+// Initialize Game
+// For now, game is set on a 4x4 grid.
+let Main = new Game(4);
+
+ReactDOM.render(
+  <Grid grid={ Main.grid } />,
+  document.getElementById('root')
+);
