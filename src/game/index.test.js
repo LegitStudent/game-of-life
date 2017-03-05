f9cf5df62656b61f9e4c697af5ccfe029a0fa230
Jasper Francisco <jasper.francisco@gmail.com>
2017-03-05 11:26:30 +0800

    Write test for evaluate method

--- src/game/index.test.js
+++ src/game/index.test.js
@@ -1,13 +1,20 @@
 import Game from './index';
 
-test('counts the right number of neighbors', () => {
-    let sample = new Game(3);
-    for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3; j++) {
-            sample.grid[i][j] = true;
-        }
-    }
+let sample = new Game(3);
 
+for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 3; j++) {
+        sample.grid[i][j] = true;
+    }
+}
+
+test('counts the right number of neighbors', () => {
     sample.grid[0][0] = false;
-    expect(sample.countNeighbors(1, 1)).toBe(7);
+    sample.grid[0][1] = false;
+    expect(sample.countNeighbors(1, 1)).toBe(6);
+});
+
+test('correctly evaluates the next generation', () => {
+    let newGrid = sample.evaluate();
+    expect(newGrid[2][2]).toBeTruthy();
 });
