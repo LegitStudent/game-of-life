fd4009ed9cef2e83a6ad50cc33bea6b2c61a188c
Jasper Francisco <jasper.francisco@gmail.com>
2017-03-05 19:26:58 +0800

    Write tests for new game code

--- src/game/index.test.js
+++ src/game/index.test.js
@@ -2,19 +2,25 @@ import Game from './index';
 
 let sample = new Game(3);
 
+sample.grid = [
+    [false, false, true],
+    [true,  false, true],
+    [false, true,  true]
+];
+
+/*
 for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
         sample.grid[i][j] = true;
     }
 }
-
+*/
 test('counts the right number of neighbors', () => {
-    sample.grid[0][0] = false;
-    sample.grid[0][1] = false;
-    expect(sample.countNeighbors(1, 1)).toBe(6);
+    expect(sample.countNeighbors(0, 0)).toBe(1);
 });
 
 test('correctly evaluates the next generation', () => {
     let newGrid = sample.evaluate();
-    expect(newGrid[2][2]).toBeTruthy();
+    
+    expect(newGrid.getCell(1, 2)).toBeTruthy();
 });
